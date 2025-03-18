c83a02e8e879728c61e46900efeef80b58ad634b
Henog123 <henrymiddleton06@gmail.com>
2025-03-17 21:46:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -22,4 +22,6 @@ def get_location():
     return jsonify(gps_location)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
